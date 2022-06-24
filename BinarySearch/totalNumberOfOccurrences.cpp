0b803b2da047fe1b0c6ce7874529866a677f091e
shivam123-dev <shivam02bhu@gmail.com>
2022-06-25 00:08:36 +0530

    Total number of occurrences

---
+++ BinarySearch/totalNumberOfOccurrences.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int count(int* arr, int start, int end) {
+    int c = 0;
+    for(int i=start; i <= end; i++) 
+        c++;
+    return c;
+}
+int find(int* arr, int n, int target) {
+    int start = 0, end = n-1;
+    int c = 0;
+    // first occurrence 
+    int first = 0, last = 0;
+    while(start<=end) {
+        int mid = start + (end - start) / 2;
+        if(arr[mid] == target) {
+            first = mid;
+            end = mid-1;
+        }
+        else if(arr[mid] < target) 
+            start = mid + 1;
+        else if(arr[mid] > target) 
+            end = mid - 1;
+    }
+    start = 0, end = n-1;
+    // last occurrence
+    while(start <= end) {
+        int mid = start + (end - start) / 2;
+        if(arr[mid] == target) {
+            last = mid;
+            start = mid + 1;
+        }
+        else if(arr[mid] < target) 
+            start = mid + 1;
+        else if(arr[mid] > target)
+            end = mid - 1;
+    }
+    return count(arr, first, last);
+}
+int main() {
+    int arr[7] = {1, 2, 3, 3, 3, 3, 5};
+    int n = sizeof(arr)/sizeof(int);
+    cout << "The total number of occurrences of 3 is: " << find(arr, n, 3) << endl;
+    return 0;
+}
