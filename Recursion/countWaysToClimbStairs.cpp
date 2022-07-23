b1bd86d83bfeb193bf2dc9c0c0d39ade5d898f9b
shivam123-dev <shivam02bhu@gmail.com>
2022-07-23 17:44:33 +0530

    Added Recursive Approach to Climbing Stairs

--- Recursion/countWaysToClimbStairs.cpp
+++ Recursion/countWaysToClimbStairs.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
+// Iterative Approach
 int countWays(int n) {
-    // base case
     if(n <= 2)
         return n;
-    // recursive relation
     int prev1 = 1, prev2 = 2, curr;
     for(int i = 3; i <= n; i++) {
         curr = prev1 + prev2;
@@ -13,6 +12,18 @@ int countWays(int n) {
     }
     return curr;
 }
+// Recursive Approach
+int countDistinctWays(int n) {
+    // base case
+    if(n < 0)
+        return 0;
+    if(n == 0)
+        return 1;
+    return countDistinctWays(n-1) + countDistinctWays(n-2);
+}
+// The above recursive approach will generate the TLE error, this is 
+// because the compiler's call stack is not that much memory efficient
+// and hence causes call stack overflow
 int main() {
     int n;
     cin >> n;
