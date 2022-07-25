bbb02e048927d6bacf40bb5b1226988d7e538303
shivam123-dev <shivam02bhu@gmail.com>
2022-07-25 18:48:42 +0530

    Reverse String using Recursion

--- Recursion/reverseString.cpp
+++ Recursion/reverseString.cpp
@@ -22,10 +22,18 @@ string reverse(string s, int i, int j) {
     swap(s[i], s[j]);
     return reverse(s, i+1, j-1);
 }
+string reve(string s, int i) {
+    if(i > s.length()-i-1) {
+        return s;
+    }
+    swap(s[i], s[s.length()-i-1]);
+    return reve(s, i+1);
+}
 int main() {
     string s;
     cin >> s;
     cout << rev(s) << endl;
     cout << reverse(s, 0, s.length()-1) << endl;
+    cout << reve(s, 0) << endl;
     return 0; 
 }
